083465cf2587ca2c7afd4c84b7a39df7f6ce4277
Arbab Ahmed <arbab.fahm@gmail.com>
2021-07-11 16:28:58 +1000

    Added timer functionality and made the code faster.

--- hackbot.py
+++ hackbot.py
@@ -1,9 +1,10 @@
 # bot.py
 import os
-import discord
 import string
 import random
 import sqlite3
+import time
+import math
 from dotenv import load_dotenv
 from discord.ext import commands
 from keep_alive import keep_alive
@@ -15,7 +16,8 @@ c.execute("""CREATE TABLE IF NOT EXISTS games(
    gamestatus INT,
    guesses INT,
    answer TEXT,
-   charset TEXT);
+   charset TEXT,
+   starttime INT);
 """)
 conn.commit()
 
@@ -58,6 +60,13 @@ def update_charset(guildid, new_charset):
     c.execute("UPDATE games SET charset=? WHERE guildid=?", (new_charset, str(guildid),))
     conn.commit()
 
+def get_starttime(guildid):
+    return c.execute("SELECT starttime FROM games WHERE guildid=?", (str(guildid),)).fetchone()[0]
+
+def update_starttime(guildid, new_starttime):
+    c.execute("UPDATE games SET starttime=? WHERE guildid=?", (new_starttime, str(guildid),))
+    conn.commit()
+
 
 @bot.command(name='hack')
 async def start_hack(ctx, *args):
@@ -78,7 +87,7 @@ async def start_hack(ctx, *args):
             return
     # start the hacking process
     if get_status(guild) is None:
-        c.execute("INSERT INTO games VALUES (?, ?, ?, ?, ?)", (str(guild), 0, arg_attempt, "", ""))
+        c.execute("INSERT INTO games VALUES (?, ?, ?, ?, ?, ?)", (str(guild), 0, arg_attempt, "", "", 0))
         conn.commit()
     if get_status(guild) == False:
         char_list = string.ascii_letters + string.digits
@@ -91,10 +100,11 @@ async def start_hack(ctx, *args):
         char_response = ""
         update_guesses(guild, arg_attempt)
         for item in array:
-            char_response += item
-        response = ("The available letters are: " + char_response + "\nYou have " + str(get_guesses(guild)) + " guesses.")
+            char_response = "".join([char_response, item])
+        response = ("The available letters are: {0}\nYou have {1} guesses.").format(char_response, str(get_guesses(guild)))
         update_charset(guild, char_response)
         update_status(guild, True)
+        update_starttime(guild, time.perf_counter())
         await ctx.send(response)
     else:
         await ctx.send("A game is already in progress!")
@@ -108,7 +118,7 @@ async def quit(ctx):
     if get_status(guild) == False:
         await ctx.send("No game is in progress!")
     else:
-        await ctx.send("Game has been stopped. Answer was: " + get_answer(guild))
+        await ctx.send("Game has been stopped. Answer was: {0}".format(get_answer(guild)))
         update_status(guild, False)
 
 @bot.event
@@ -121,7 +131,10 @@ async def on_message(message):
         if get_status(guild) == True and len(message.content) == 3:
             correct = ""
             if message.content == get_answer(guild):
-                await message.channel.send("Correct!")
+                diff = time.perf_counter() - get_starttime(guild)
+                min = math.floor(diff/60)
+                sec = diff % 60
+                await message.channel.send("Correct!\nTime taken: {}:{:0>2d}".format(str(min), int(sec)))
                 update_status(guild, False)
             else:
                 charset = get_charset(guild)
@@ -131,19 +144,19 @@ async def on_message(message):
                         for j in range(3):
                             if message.content[i] == get_answer(guild)[j]:
                                 if i == j:
-                                    correct += "!"
+                                    correct = "".join([correct, "!"])
                                 else:
-                                    correct += "?"
+                                    correct = "".join([correct, "?"])
                     else:
                         await message.channel.send("One or more of the characters is not part of the given character set.")
                         return
                 update_guesses(guild, get_guesses(guild) - 1)
                 if get_guesses(guild) == 0:
-                    await message.channel.send("You have run out of guesses. The answer was: " + get_answer(guild))
+                    await message.channel.send("You have run out of guesses. The answer was: {0}".format(get_answer(guild)))
                     update_status(guild, False)
                     return
                 correct = ("".join(sorted(correct)))[::-1] # reverse this string to fit with tachyons notation
-                await message.channel.send("**[" + correct + "]**" + "\nGuesses left: " + str(get_guesses(guild)))
+                await message.channel.send("**[{0}]**\nGuesses left: {1}".format(correct, str(get_guesses(guild))))
         await bot.process_commands(message)
 
 bot.remove_command("help")
